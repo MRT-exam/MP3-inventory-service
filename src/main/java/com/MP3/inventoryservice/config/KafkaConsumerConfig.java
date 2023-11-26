a034cf4934c9fd36ca5ee6e97353e2f11ba5f9e2
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-11-26 15:23:37 +0100

    refactor entire kafka config and mapping

--- src/main/java/com/MP3/inventoryservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/MP3/inventoryservice/config/KafkaConsumerConfig.java
@@ -2,7 +2,11 @@ package com.MP3.inventoryservice.config;
 
 import com.MP3.inventoryservice.Event.OrderPlacedEvent;
 import com.MP3.inventoryservice.Event.ResupplyDeliveryEvent;
+import com.MP3.inventoryservice.dto.OrderedProductDto;
+import com.MP3.inventoryservice.dto.ResupplyProductDto;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +15,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -25,63 +33,43 @@ public class KafkaConsumerConfig {
     @Value(value = "inventoryService")
     private String groupId;
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
+    public Map<String, Object> consumerConfig() {
+        HashMap<String, Object> props = new HashMap();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(props);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        return props;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-
-    public ConsumerFactory<String, OrderPlacedEvent> orderPlacedConsumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(props,
-                (Deserializer) new StringDeserializer(),
-                new JsonDeserializer<>(OrderPlacedEvent.class));
+    public RecordMessageConverter multiTypeConverter() {
+        StringJsonMessageConverter converter = new StringJsonMessageConverter();
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
+        typeMapper.addTrustedPackages("*");
+        Map<String, Class<?>> mappings = new HashMap<>();
+        mappings.put("orderPlacedEvent", OrderPlacedEvent.class);
+        mappings.put("orderLineDto", OrderedProductDto.class);
+        mappings.put("resupplyDeliveryEvent", ResupplyDeliveryEvent.class);
+        mappings.put("resupplyOrderLineDto", ResupplyProductDto.class);
+        typeMapper.setIdClassMapping(mappings);
+        converter.setTypeMapper(typeMapper);
+        return converter;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, OrderPlacedEvent>
-    orderPlacedKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, OrderPlacedEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(orderPlacedConsumerFactory());
-        return factory;
-    }
-
-    public ConsumerFactory<String, ResupplyDeliveryEvent> resupplyDeliveryConsumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(props,
-                (Deserializer) new StringDeserializer(),
-                new JsonDeserializer<>(ResupplyDeliveryEvent.class));
+    public ConsumerFactory<String, Object> multiTypeConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, ResupplyDeliveryEvent>
-    resupplyDeliveryKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, ResupplyDeliveryEvent> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(resupplyDeliveryConsumerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, Object> multiTypeKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(multiTypeConsumerFactory());
+        factory.setMessageConverter(multiTypeConverter());
         return factory;
     }
 }
--- src/main/java/com/MP3/inventoryservice/config/KafkaProducerConfig.java
+++ src/main/java/com/MP3/inventoryservice/config/KafkaProducerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,31 +20,25 @@ public class KafkaProducerConfig {
     @Value(value = "${SPRING_KAFKA_BOOTSTRAP_SERVERS}")
     private String bootstrapAddress;
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> props = new HashMap<>();
+    public Map<String, Object> producerConfig() {
+        HashMap<String, Object> props = new HashMap();
+
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonDeserializer.TYPE_MAPPINGS,
+                "resupplyRequestedEvent:com.MP3.inventoryService.event.ResupplyRequestedEvent, resupplyProductDto:com.MP3.inventoryService.dto.ResupplyProductDto");
+
+        return props;
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
     }
 
     @Bean
-    public ProducerFactory<String, ResupplyRequestedEvent> resupplyRequestedProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, ResupplyRequestedEvent> resupplyRequestedKafkaTemplate() {
-        return new KafkaTemplate<>(resupplyRequestedProducerFactory());
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
