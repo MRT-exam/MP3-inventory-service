e6a9f673b879f2eff8134172d0c745e700b816d1
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-11-26 11:41:01 +0100

    minor changes

--- src/main/java/com/MP3/inventoryservice/producer/ResupplyRequestedProducer.java
+++ src/main/java/com/MP3/inventoryservice/producer/ResupplyRequestedProducer.java
@@ -2,11 +2,13 @@ package com.MP3.inventoryservice.producer;
 
 import com.MP3.inventoryservice.Event.ResupplyRequestedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 @Service
-@RequiredArgsConstructor
 public class ResupplyRequestedProducer {
+
+    @Autowired
     private KafkaTemplate<String, ResupplyRequestedEvent> kafkaTemplate;
 
     public void produce(ResupplyRequestedEvent event) {
